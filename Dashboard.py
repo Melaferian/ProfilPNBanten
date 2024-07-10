e31925b27974dfee961c0fe62ff71ca85217e7ce
Mela Feriani Simanjuntak <93712013+Melaferian@users.noreply.github.com>
2024-07-10 16:02:05 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # Set the title of the app
-st.title("Dashboard Profil Piutang Negara Kanwil DJKN Banten")
+st.write("### Dashboard Profil Piutang Negara Kanwil DJKN Banten")
 
 # URL of the dashboard to be embedded
 dashboard_url = "https://app.powerbi.com/view?r=eyJrIjoiZDNhNWEyZTctY2FlMi00NDQ3LWFkNTUtNzEwMmEwNzc2NDU2IiwidCI6ImVkNmZiMzY2LTgzMjItNDZmMy05MTVlLWM0ZDAzN2E0NTRhOSIsImMiOjEwfQ%3D%3D"
